1b1d6efe36e8c6d677dc411ab4d0c9e8465dcee3
Kevin Ashworth <kevinashworth@yahoo.com>
2024-11-11 17:30:44 -0800

    Initial commit
    https://docs.bsky.app/docs/starter-templates/clients

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "cdn.bsky.app",
+    }],
+  },
   /* config options here */
 };
 
